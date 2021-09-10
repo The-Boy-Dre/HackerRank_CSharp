d2c73ef94545947faee2fed3096b155e2742bc63
Dondre Campbell <Dondrexcampbell@gmail.com>
2021-09-10 09:41:02 -0400

    first

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //char[] charsToTrim = {'.',','};
+            //String filepath = ".,.,Ballon,,..".Trim('.',',');
+            Console.WriteLine(".,.,Ballon,,..".Trim('.',',')); 
+
+            	int[] nums = { 1, 2, 3, 4 };
+		
+		        Console.WriteLine(String.Join("connect",nums));
+                 
+
+        }
+    }
+}
+  
